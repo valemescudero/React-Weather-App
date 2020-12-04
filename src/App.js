c809d44a888e3dd75d68f25d3fe8609a15cf451e
Evadne Meraki <valemescudero@gmail.com>
2020-12-03 21:23:14 -0300

    api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 
 const api = {
-  base: "https://api.openweathermap.org/data/2.5/"
+  base: "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
